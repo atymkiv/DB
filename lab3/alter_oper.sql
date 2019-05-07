a9840cd3aebd1a6ea30699ee8b9d3add92c225d3
atymkiv <a.tymkiv99@gmail.com>
2019-05-07 09:42:27 +0300

    changed lab2 and lab3 sql

--- lab3/alter_oper.sql
+++ lab3/alter_oper.sql
@@ -1,10 +1,26 @@
 alter table real_estate.user 
 add column modifyDate date;
 
+
+
+alter table real_estate.user 
+add column surname nvarchar(50) NOT NULL;
+
 alter table real_estate.user 
 modify column modifyDate datetime;
+
+alter table real_estate.user 
+modify column login nvarchar(50) NOT NULL unique;
 
 alter table real_estate.user
 add constraint UQ_Email unique (email);
 
 alter table real_estate.user drop column modifyDate;
+
+
+alter table real_estate.user_operation
+modify column userID int unsigned not null unique,
+modify column estateID int unsigned not null unique;
+
+
+
