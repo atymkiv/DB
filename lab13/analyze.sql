367d8797b7dddc8e81f206a22bbfc7d7e57161e3
Andriy <a.tymkiv99@gmail.com>
2019-06-06 10:15:46 +0300

    lab13

--- lab13/analyze.sql
+++ lab13/analyze.sql
@@ -1,3 +1,19 @@
 use real_estate;
-
+#Індекси таблиць user i estate
+SHOW INDEX FROM estate;
 SHOW INDEX FROM user;
+
+#Cтворимо індекс для поля login на таблиці user
+CREATE INDEX userINDX3 ON user (userID, login);
+SHOW INDEX FROM user;
+
+#Виконаємо аналіз виконання складного запиту (Вивести операції над нерухомістю з юзерами для яких роль User or Moderator) з однієї з попередніх робіт використовуючи EXPLAIN
+EXPLAIN SELECT  real_estate.estate.estateID, real_estate.estate.type, real_estate.user_operation.operationID, real_estate.user.login, real_estate.role.role_name from real_estate.estate, real_estate.role,
+(real_estate.user INNER JOIN real_estate.user_role) inner join real_estate.user_operation on real_estate.user.userID = real_estate.user_role.userID
+and real_estate.user_operation.userID = user.userID where real_estate.user_role.roleID = real_estate.role.roleID and real_estate.role.role_name in ('User', 'Moderator')
+                                                      and real_estate.user_operation.estateID = real_estate.estate.estateID;
+
+EXPLAIN SELECT STRAIGHT_JOIN  real_estate.estate.estateID, real_estate.estate.type, real_estate.user_operation.operationID, real_estate.user.login, real_estate.role.role_name from real_estate.estate, real_estate.role,
+(real_estate.user INNER JOIN real_estate.user_role) inner join real_estate.user_operation on real_estate.user.userID = real_estate.user_role.userID
+and real_estate.user_operation.userID = user.userID where real_estate.user_role.roleID = real_estate.role.roleID and real_estate.role.role_name in ('User', 'Moderator')
+                                                      and real_estate.user_operation.estateID = real_estate.estate.estateID;
--- lab7/queries.sql
+++ lab7/queries.sql
@@ -9,10 +9,11 @@ from real_estate.user RIght JOIN real_estate.user_operation ON user.userID = use
 SELECT real_estate.user.login, real_estate.role.role_name from real_estate.role, real_estate.user inner join real_estate.user_role on real_estate.user_role.userID = real_estate.user.userID
 where real_estate.user_role.roleID = real_estate.role.roleID and real_estate.role.roleID in (select roleID from real_estate.role where role_name = 'User');
 
+#Вивести операції над нерухомістю з юзерами для яких роль User or Moderator
 SELECT  real_estate.estate.estateID, real_estate.estate.type, real_estate.user_operation.operationID, real_estate.user.login, real_estate.role.role_name from real_estate.estate, real_estate.role,
 (real_estate.user INNER JOIN real_estate.user_role) inner join real_estate.user_operation on real_estate.user.userID = real_estate.user_role.userID
 and real_estate.user_operation.userID = user.userID where real_estate.user_role.roleID = real_estate.role.roleID and real_estate.role.role_name in ('User', 'Moderator')
-                                                      and real_estate.user_operation.estateID = real_estate.estate.estateID
+                                                      and real_estate.user_operation.estateID = real_estate.estate.estateID;
 
 SELECT userID, login from real_estate.user
 where not exists(select * from real_estate.user_operation where real_estate.user_operation.userID = real_estate.user.userID)
